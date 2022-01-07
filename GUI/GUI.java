c446a1ee505671102de4d1d8982b83b9b7d66a8c
Moira Clancy <70659975+Moira7@users.noreply.github.com>
2022-01-07 17:41:33 +0000

    added to gui class

--- GUI/GUI.java
+++ GUI/GUI.java
@@ -2,9 +2,26 @@ package battlesystem;
 
 import javax.swing.*;
 
+import battlesystem.Entity;
+
 public class GUI{
 
-    public GUI(Entity current, Entity target){
+  //use entity instead of superhero
+  public Entity opponet;
+
+    public GUI(){
         JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+    }
+
+    //This screen will let the user select their opponet
+    public void getOpponet(){
+      //how many opponets?
+    }
+
+    //this screen will let the user choise a action
+    public void getAction(){
+      //how many actions 
     }
 }
