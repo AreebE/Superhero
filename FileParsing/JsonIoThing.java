fb1277103bd6fbb6c0b709fcba5f2fdb1a3334c1
paulograbnsd <70658913+paulo-grab-nsd@users.noreply.github.com>
2022-01-07 17:36:45 +0000

    Fixed stuff

--- FileParsing/JsonIoThing.java
+++ FileParsing/JsonIoThing.java
@@ -2,51 +2,61 @@ package battlesystem;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import java.io.FileReader;
-import java.util.Iterator;
 import java.util.*;
+import java.nio.file.*;
 import java.io.*;
+import Game.*;
 
 class JsonIoThing {
   File thisFile;
+  String thisFileName;
+  Path thisFilePath;
 
   FileWriter fw;
   BufferedWriter bw;
+  FileReader fr;
+  
 
   public JsonIoThing(String fileName) {
-
-    this.thisFile = new File("save.json");
-
+    this.thisFile = new File(fileName);
+    this.thisFileName = fileName;
+    this.thisFilePath = Path.of(thisFileName);
     try {
       // fs = new Scanner(thisFile);
       fw = new FileWriter(thisFile);
       bw = new BufferedWriter(fw);
+      fr = new FileReader(thisFile);
     } catch (Exception Ex) {
-      System.out.println("HEY exce in JsonIoThing init: " + Ex);
+      Ex.printStackTrace();
+      System.exit(1);
     }
 
   }
 
   public void saveSuperheroArr(ArrayList<Entity> heros) {
-    
-    System.out.println("Savin");
-
-
+    System.out.println("\n\u001B[33m"+"Savin Superheros... "+"\u001B[0m\n ");
     Gson gson = new GsonBuilder().setPrettyPrinting().create(); 
-    String json = gson.toJson(heros.get(0));
-    System.out.println("\n "+json);
-    Entity a = gson.fromJson(json,Entity.class);
-    System.out.println("NAMES "+a.getName());
-
-  }
-
-  public void loadSuperheroArr() {
-    // there is gson.fromJson(Json,class)
-  }
-
-  public void printStringArrToCons(String[] in) {
-    for (int i = 0; i < in.length; i++) {
-      System.out.println("Printing " + in[i] + " at " + i);
+    String json = gson.toJson(heros);
+    try{
+      Files.writeString(thisFilePath,json);
+      //System.out.println("\n "+json);
+    }catch(IOException e){
+      e.printStackTrace();
+      System.exit(1);
     }
+    
+
+  }
+
+  public Entity[] loadSuperheroArr() {
+    System.out.println("\n\u001B[33m"+"loadin Superheros... "+"\u001B[0m\n ");
+    Entity[] out = new Entity[1];
+    try{
+      out= new Gson().fromJson(Files.readString(thisFilePath),Entity[].class);
+    }catch(IOException e){
+      e.printStackTrace();
+      System.exit(1);
+    }
+    return out;
   }
 }
