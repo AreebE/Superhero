5f0ce73c976f3b64cde4b1d45ea4144b7df19298
Moira Clancy <70659975+Moira7@users.noreply.github.com>
2022-01-14 17:19:30 +0000

    added more to gui

--- GUI.java
+++ GUI.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
 
-  public GUI(){
+
+  public void GUI(){
 
     //5 abilites per superhero + pass turn
+    Boolean start = false;
 
     JFrame frame = new JFrame();
     frame.setSize(200, 400);
@@ -13,16 +17,36 @@ public class GUI {
 
     welcomeScreen.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
 
-    JTextArea welcome = new JTextArea("Hello! Welcome to the Superhero Project. Down in the console please enter the questions asks. When finshed please select the Next Button.");
+    welcomeScreen.setLayout(new GridLayout(4, 0));
+    JTextArea welcome = new JTextArea("Hello! Welcome to the Superhero Project. ");
+    JTextArea w2 = new JTextArea("Down in the console please enter the questions asks.");
+    JTextArea w3 = new JTextArea(" When finshed please select the Next Button.");
     //look into how to resize this...
     welcome.setEditable(false);
+    w2.setEditable(false);
+    w3.setEditable(false);
     welcomeScreen.add(welcome);
+    welcomeScreen.add(w2);
+    welcomeScreen.add(w3);
 
+    JButton next = new JButton("NEXT");
+    welcomeScreen.add(next);
+    next.addActionListener(this);
+    
     frame.add(welcomeScreen);
-    
-    
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
+
+    while(start == false){
+      System.out.println("this works so far");
+      start = true;
+
+    }
+
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e){
 
   }
 }
