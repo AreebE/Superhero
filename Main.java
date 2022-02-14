653fe7a73060ef25dbfb500fdeed8d75898d78d8
Moira Clancy <70659975+Moira7@users.noreply.github.com>
2022-02-14 17:31:28 +0000

    Something in the code is broken -- its not letting you select a target

--- Main.java
+++ Main.java
@@ -6,8 +6,6 @@ import battlesystem.*;
 //should we make like a "main menu" type of thing so that
 //it would be easier to do stuff like change the file or 
 //create new heros and/or abilities and choose to save and load...
- 
-
 
 class Main {
   public static void main(String[] args) {
